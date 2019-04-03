44fdf263ea87b1c4be61aea82a50f5a37334260f
leishiguang <leishiguang@outlook.com>
2019-04-03 08:50:46 +0800

    增加启动后的说明页

---
+++ ops-web/src/main/java/com/supermap/runner/WelcomeRunner.java
@@ -0,0 +1,33 @@
+package com.supermap.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 展示一些欢迎信息，或是增加版本号……
+ */
+@Slf4j
+@Component
+@Order(value = 99)
+public class WelcomeRunner implements CommandLineRunner {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("#{ ('dev' eq '${spring.profiles.active}') && '${debug}'}")
+    private boolean isDebug;
+
+    @Value("${spring.profiles.active}")
+    private String environmentType;
+
+    @Override
+    public void run(String... args){
+        if(isDebug){
+            log.info("开启：调试模式 ----------------------------!");
+        }
+        log.info("启动完成，当前环境：{}，端口号：{}",environmentType,serverPort);
+    }
+}
